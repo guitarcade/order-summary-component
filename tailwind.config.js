59334d53de90895932fcffc4e6b837378f96f4ee
Arik Cohen <me@arikcohen.com>
2023-05-26 11:32:18 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    colors: {
+      // 'dark-cyan-400':'hsl(158, 36%, 37%)',
+      // 'dark-cyan-700':'hsl(158, 36%, 17%)',
+      // 'cream':'hsl(30, 38%, 92%)',
+      // 'very-dark-blue': 'hsl(212, 21%, 14%)',
+      // 'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+      // 'white': 'hsl(0, 0%, 100%)'
+    },
+    extend: {
+      fontFamily: {
+        // 'montserrat'  : ['Montserrat', 'sans-serif'],
+        // 'fraunces'  : ['Fraunces', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
